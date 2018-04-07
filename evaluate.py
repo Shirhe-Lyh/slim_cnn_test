c93be3f3086efa4a9be31eaeb1bcdfe17f854d3f
Shirh Lyh <33980058+Shirhe-Lyh@users.noreply.github.com>
2018-04-07 23:37:33 +0800

    Update evaluate.py

--- evaluate.py
+++ evaluate.py
@@ -10,9 +10,7 @@ Created on Mon Apr  2 14:02:05 2018
 """Evaluate the trained CNN model.
 
 Example Usage:
-
 ---------------
-
 python3 evaluate.py \
 
     --frozen_graph_path: Path to model frozen graph.
